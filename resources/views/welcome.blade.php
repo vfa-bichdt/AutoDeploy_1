31adf3f572083461f2f28474fe1dbbfe77ace57a
Bichdt <bichdt@vitalify.asia>
2020-05-12 20:29:54 +0700

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
             </div>
             <div class="collapse navbar-collapse justify-content-end" id="navigation">
               <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link smooth-scroll" href="#about">About+1</a></li>
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#about">About</a></li>
                 <li class="nav-item"><a class="nav-link smooth-scroll" href="#skill">Skills</a></li>
                 <li class="nav-item"><a class="nav-link smooth-scroll" href="#portfolio">Portfolio</a></li>
                 <li class="nav-item"><a class="nav-link smooth-scroll" href="#experience">Experience</a></li>
